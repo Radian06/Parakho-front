e60820b11b09f5d03da9889f1725fc20dc95cd6a
Radian06 <aksen05aksen@gmail.com>
2025-10-29 23:01:11 +0900

    📄 :: 헤더 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Main from "./pages/Main";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header_page">
+      test
+    </div>
+  );
+};
+
+export default Header;
