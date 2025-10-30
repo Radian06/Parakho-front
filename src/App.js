267f4081b050c87cc86a9f893d832f92051d4a66
Radian06 <aksen05aksen@gmail.com>
2025-10-30 13:07:44 +0900

    📄 :: 장소 목록 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import MapCheck from "./pages/MapCheck";
+import LocationList from "./pages/LocationList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/map-check" element={<MapCheck />} />
+        <Route path="/location-list" element={<LocationList />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/LocationList.jsx
@@ -0,0 +1,11 @@
+import "../styles/LocationList.css";
+
+const LocationList = () => {
+  return (
+    <div className="LocationList_page">
+      <div className="LocationList_title">독립운동 관련 장소 리스트</div>
+    </div>
+  );
+};
+
+export default LocationList;
