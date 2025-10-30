35feb5a43fce1715c7d065e35aa95d4d238cc02a
Radian06 <aksen05aksen@gmail.com>
2025-10-30 14:08:34 +0900

    📄  :: 장소 목록 페이지 생성

---
+++ src/data/locationData.js
@@ -0,0 +1,23 @@
+export const locationData = [
+  {
+    id: 1,
+    name: "임청각",
+    description: "안동시 법흥동에 있는 독립운동가 이상룡 선생의 생가입니다.",
+    address: "경상북도 안동시 법흥동",
+    position: [36.5652131, 128.7448513],
+  },
+  {
+    id: 2,
+    name: "향산고택",
+    description: "조선 후기 전통 한옥으로, 안동시 풍산읍에 위치한 고택입니다.",
+    address: "경상북도 안동시 풍산읍",
+    position: [36.6222, 128.8565],
+  },
+  {
+    id: 3,
+    name: "독립운동기념관",
+    description: "독립운동가 관련 자료를 전시한 기념관입니다.",
+    address: "서울특별시 종로구",
+    position: [37.572, 126.979],
+  },
+];
--- src/pages/LocationList.jsx
+++ src/pages/LocationList.jsx
@@ -1,9 +1,34 @@
 import "../styles/LocationList.css";
+import { locationData } from "../data/locationData";
 
 const LocationList = () => {
   return (
     <div className="LocationList_page">
-      <div className="LocationList_title">독립운동 관련 장소 리스트</div>
+      <div className="LocationList_title">독립 관련 장소 리스트</div>
+      <div className="LocationList_listBox">
+        <table className="LocationList_table">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>명칭</th>
+              <th>관련 독립운동가</th>
+              <th>주소</th>
+              <th>시설</th>
+            </tr>
+          </thead>
+          <tbody>
+            {locationData.map((loc) => (
+              <tr key={loc.id}>
+                <td>{loc.id}</td>
+                <td>{loc.name}</td>
+                <td>{loc.relatedPerson}</td>
+                <td>{loc.address}</td>
+                <td>{loc.facility}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
