54fee55d2af6ffa88f960feab8205470d04482b2
Radian06 <aksen05aksen@gmail.com>
2025-10-30 09:49:51 +0900

    🎨 :: Main 페이지 -> MapCheck 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Main from "./pages/Main";
+import MapCheck from "./pages/MapCheck";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/map-check" element={<MapCheck />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/MapCheck.jsx
@@ -0,0 +1,13 @@
+import "../styles/MapCheck.css";
+import Map from "../components/Map/Map";
+
+const MapCheck = () => {
+  return (
+    <div className="MapCheck_page">
+      <div className="MapCheck_title">독립운동가 관련 위치</div>
+      <Map />
+    </div>
+  );
+};
+
+export default MapCheck;
