ad07e857a8508e352c42163d3eff687eaea7eba2
Radian06 <aksen05aksen@gmail.com>
2025-10-30 09:27:12 +0900

    ⚡️:: 헤더 네비바 추가 중

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ import "../styles/Header.css";
 const Header = () => {
   return (
     <div className="header_page">
-      test
+      <div className="header_nav_box">
+        <div className="header_nav">지도 확인</div>
+        <div className="header_nav">지도 확인</div>
+        <div className="header_nav">지도 확인</div>
+      </div>
     </div>
   );
 };
